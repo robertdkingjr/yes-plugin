99d0a082834f31e04109a724744181b714801446
robertdkingjr <robertdkingjr@gmail.com>
2021-04-07 18:52:45 -0500

    Init gitignore, main plugin file

---
+++ yes.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: YES plugin
+ * Plugin URI: https://yes.site/yes
+ * Description: Demo plugin for education purposes
+ * Version: 1.0
+ * Author: robert
+ * Author URI: https://yes.site/robert
+*/
